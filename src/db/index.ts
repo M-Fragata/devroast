cd2b46dcfc7168eeb7b9db397765bb2ac2e89745
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-19 19:33:30 -0300

    .

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL ?? 'postgres://devroast_user:devroast_password@localhost:5432/devroast';
+const connectionString = process.env.DATABASE_URL
 
-const client = postgres(connectionString, { 
+const client = postgres(connectionString!, { 
   idle_timeout: 20,
   max: 1,
 });
