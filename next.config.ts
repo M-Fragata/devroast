d1027a73da1a9dce00dbe054f7905343b1512971
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-19 21:11:03 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	serverExternalPackages: ["postgres", "drizzle-orm", "@prisma/client"],
+	experimental: {
+		optimizePackageImports: ["@google/generative-ai"],
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 };
 
 export default nextConfig;
